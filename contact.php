eb661384b7e580eff2d6363fff49b0c12cc5be22
vcoquinot <vcoquinot@gmail.com>
2017-12-22 12:13:43 +0100

    added contact

---
+++ contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sortirgratos/contact</title>
+    <link href="main.css"  type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+    <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <section id="introduction-contact" class="container'fluid">
+      <?php include ("menu-logo-blanc.php");?>
+      <div class="col-md-12 row text-center">
+      <h1>Nous contacter</h1>
+      <section id="formulaire-creation-compte" class="container">
+      <form class="form-horizontal">
+        <div class="form-group row"><!--voir si formGroupInputMedium necessaire ?? -->
+            <div class="col-md-offset-3 col-md-3">
+              <input class="form-control" type="nom" id="formGroupInputNom" placeholder="Nom">
+            </div>
+            <div class="col-md-3">
+              <input class="form-control" type="email" id="formGroupInputEmail" placeholder="Mail">
+            </div>
+        </div>
+      <form class="form-horizontal">
+        <div class="form-group row ">
+          <div class="col-md-offset-3 col-md-6">
+            <textarea class="form-control" id="FormControlTextarea" placeholder="Votre message" rows="8"></textarea><!--agrandir largeur textarea ?? -->
+          </div>
+
+        </div>
+         <button type="submit" class="btn btn-light">Envoyer</button>
+      </form>
+      </form>
+      <?php include ("footer.php");?>
+  </body>
+  </html>

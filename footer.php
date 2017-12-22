97b7440b29ba4c20773d0edf5b70dcf8b707f35f
vcoquinot <vcoquinot@gmail.com>
2017-12-22 11:57:55 +0100

    adit footer marc

--- footer.php
+++ footer.php
@@ -1,26 +1,14 @@
 <footer class="container-fluid">
     <div class="row text-center">
-        <div class="col-md-2 ">
-            <a href="#"><h4>Inscription Newsletter</h4></a>
+
+        <div class="col-md-offset-1 col-md-10 row-text-center">
+            <a href="#"><h4>Inscription Newsletter</h4></a><a href="qui-sommes-nous.php"><h4>Qui sommes nous ?</h4></a> <a href="#"><h4>Contact</h4></a><a href="mentions-legales.php"><h4>Mentions Légales/CGU</h4></a><a href="#"><h4>Plan du Site</h4></a>
         </div>
+
         <div class="col-md-2">
-            <a href="qui-sommes-nous.php"><h4>Qui sommes nous ?</h4></a>
-        </div>
-        <div class="col-md-2">
-            <a href="#"><h4>Contact</h4></a>
-        </div>
-        <div class="col-md-2">
-            <a href="mentions-legales.php"><h4>Mentions Légales/CGU</h4></a>
-        </div>
-        <div class="col-md-2">
-            <a href="#"><h4>Plan du Site</h4></a>
-        </div>
-        <div class="col-md-1">
             <a href="#"><i class="fa fa-facebook-square fa-3x"></i></a>
         </div>
-        <div class="col-md-1">
-            <a href="#"><i class="fa fa-linkedin-square fa-3x"></i></a>
-        </div>
+
         <div class="col-md-12 row-text-center">
             <h6>Copyright ©2017 by Team sortirgratos.fr / All rights reserved</h6>
         </div>

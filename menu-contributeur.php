9e6256beeaa3106bd0c472729e112ae16c01780b
vcoquinot <vcoquinot@gmail.com>
2017-12-26 20:40:52 +0100

    created menu contributeur

---
+++ menu-contributeur.php
@@ -0,0 +1,12 @@
+<header class="container-fluid">
+    <div class="row">
+        <div class="col-md-8">
+            <a class="navbar-brand" href="index.html"></a>
+            <a href="index.php" title="Page d'accueil sortirgratos.fr"><img src="images/logo.png" alt= "logo"></a>
+        </div>
+        <div class ="col-md-offset-1 col-md-3">
+            <p>nom contrib.</p>
+            <button type="button" class="btn btn-light">Déconnexion</button>
+        </div>
+    </div> 
+</header>

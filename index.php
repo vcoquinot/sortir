a47655b047b162cb2e51321829af29b1e7105c04
vcoquinot <vcoquinot@gmail.com>
2017-12-26 19:28:49 +0100

    added link fontawesome

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
 <!-- Bootstrap -->
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->

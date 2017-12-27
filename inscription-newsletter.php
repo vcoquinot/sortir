84d6f62655d5d3e7da47059c52b507c28e5b6db8
vcoquinot <vcoquinot@gmail.com>
2017-12-27 18:33:38 +0100

    --

--- inscription-newsletter.php
+++ inscription-newsletter.php
@@ -1,5 +1,5 @@
           <!-- Inscription newsletter -->
-          <section id="newsletter" class="container">  
+    <section id="newsletter" class="container">  
       <div class="label-group row">
         <span class="custom-control-description col-md-offset-2 col-md-2"><strong>Inscription à la newsletter</strong></span>
         <label class="custom-control custom-radio col-md-1">

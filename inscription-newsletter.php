88f9e57cb08ed74fbbc7b4ddeb5f678131cb3086
vcoquinot <vcoquinot@gmail.com>
2017-12-27 19:38:35 +0100

    adio checked

--- inscription-newsletter.php
+++ inscription-newsletter.php
@@ -8,7 +8,7 @@
             <span class="custom-control-description">OUI</span>  
           </label>
         <label class="custom-control custom-radio col-md-1">
-          <input name="radio" type="radio" class="custom-control-input">
+          <input name="radio" type="radio" class="custom-control-input" checked>
           <span class="custom-control-indicator"></span>
           <span class="custom-control-description">NON</span>  
         </label>
@@ -17,7 +17,7 @@
       <div class="label-group row">
         <span class="custom-control-description col-md-offset-2 col-md-1"><strong>Périodicité</strong></span>
         <label class="col-md-2">
-          <input name="radio" type="radio" class="custom-control-input">
+          <input name="radio" type="radio" class="custom-control-input" checked>
           <span class="custom-control-indicator"></span>
           <span class="custom-control-description ">Hebdomadaire</span>
         </label>

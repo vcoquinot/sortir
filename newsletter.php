f917bdf7dc8da0962dbcfc622384e12664ab6f0c
vcoquinot <vcoquinot@gmail.com>
2017-12-28 11:20:44 +0100

    file for include

--- newsletter.php
+++ newsletter.php
@@ -1,7 +1,7 @@
           <!-- Inscription newsletter -->
     <section id="newsletter" class="container">  
       <div class="label-group row">
-        <span class="custom-control-description col-md-offset-2 col-md-2"><strong>Inscription à la newsletter</strong></span>
+        <span class="custom-control-description col-md-offset-2 col-md-4 row text-center"><strong>M'inscrire à la newsletter</strong></span>
         <label class="custom-control custom-radio col-md-1">
           <input name="radio" type="radio" class="custom-control-input">
             <span class="custom-control-indicator"></span>
@@ -15,16 +15,16 @@
       </div>
         <!-- Périodicité  newsletter -->
       <div class="label-group row">
-        <span class="custom-control-description col-md-offset-2 col-md-1"><strong>Périodicité</strong></span>
+        <span class="custom-control-description col-md-offset-2 col-md-4 row text-center"><strong>Recevoir la newsletter</strong></span>
         <label class="col-md-2">
           <input name="radio" type="radio" class="custom-control-input" checked>
           <span class="custom-control-indicator"></span>
-          <span class="custom-control-description ">Hebdomadaire</span>
+          <span class="custom-control-description ">chaque semaine</span>
         </label>
         <label class="col-md-2">
           <input name="radio" type="radio" class="custom-control-input">
           <span class="custom-control-indicator"></span>
-          <span class="custom-control-description ">Chaque événement</span>
+          <span class="custom-control-description ">à chaque nouvel événement</span>
         </label>
       </div>
     </section>

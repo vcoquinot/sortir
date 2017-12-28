c0e4de165f1aa078e71e5b00bd8f26be9d838495
vcoquinot <vcoquinot@gmail.com>
2017-12-28 10:28:58 +0100

    added link newsletter

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="row text-center">
 
         <div class="col-md-12 row-text-center">
-            <a href="#"><h4>Inscription Newsletter</h4></a><a href="qui-sommes-nous.php"><h4>Qui sommes nous ?</h4></a> <a href="contact.php"><h4>Contact</h4></a><a href="mentions-legales.php"><h4>Mentions Légales/CGU</h4></a><a href="#"><h4>Plan du Site</h4></a><a href="#"><i class="fa fa-facebook-square fa-lg"></i></a>
+            <a href="iscription-newsletter.php"><h4>Inscription Newsletter</h4></a><a href="qui-sommes-nous.php"><h4>Qui sommes nous ?</h4></a> <a href="contact.php"><h4>Contact</h4></a><a href="mentions-legales.php"><h4>Mentions Légales/CGU</h4></a><a href="#"><h4>Plan du Site</h4></a><a href="#"><i class="fa fa-facebook-square fa-lg"></i></a>
         </div>
 
     </div>

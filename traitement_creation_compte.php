f2c2113a3f0ee54e82cc3460bda922660ba4e9fd
vcoquinot <vcoquinot@gmail.com>
2018-01-10 15:41:38 +0100

    psw = psw_confirm

--- traitement_creation_compte.php
+++ traitement_creation_compte.php
@@ -16,8 +16,36 @@
 <pre>
     <?php
 print_r($_POST);
-    ?>
+?>
 </pre>
+
+    <?php //TODO mysql_real_escape_string
+    if(isset($_POST['pseudo'])) 
+    { 
+        $pseudo = $_POST['pseudo'];
+        $email= $_POST['email'];
+        $nom = $_POST['nom'];
+        $prenom= $_POST['prenom'];
+        $psw= $_POST['psw'];
+        $psw_confirm = $_POST['psw_confirm'];
+        $type= $_POST['type'];
+        $adresse= $_POST['adresse'];
+        $cp= $_POST['cp'];
+        $ville= $_POST['ville'];
+        $tel= $_POST['tel'];
+    }
+
+    //vérification password et password_confirm identiques
+    if($psw_confirm != $psw ) 
+    {
+        echo "les deux mots de passe doivent être identiques <br/>";
+    }
+    else{
+        echo "Votre compte a bien été créé";
+    }
+    ?>
+
+
 
 </body>
 </html>

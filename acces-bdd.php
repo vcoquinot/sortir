e2e40ca26d138129e6037685450a96d06fb618e3
vcoquinot <vcoquinot@gmail.com>
2018-01-16 11:16:40 +0100

    commented inclde to access

--- acces-bdd.php
+++ acces-bdd.php
@@ -1,9 +1,9 @@
 <?php
-try{
+/*try{
   $bdd =new PDO('mysql:host=localhost;dbname=sortirgratos;charset=utf8', 'root' , '');
 }
 catch(Exception $e)
 {
   die('Erreur : '.$e->getMessage());
-}
+}*/
 ?>

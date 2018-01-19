1c8578a02de0830778d73b203dcb207851d168ae
vcoquinot <vcoquinot@gmail.com>
2018-01-19 14:58:44 +0100

    added $_login

--- menu-contributeur.php
+++ menu-contributeur.php
@@ -6,11 +6,6 @@
             <a href="index.php" title="Page d'accueil sortirgratos.fr"><img class="anim_logo" src="images/logo.png" alt= "logo"></a>
         </div>
         <div class="col-md-offset-3 col-md-5 row text-right">
-
-            <!--TODO ajouter variable php contributeur connecté-->
-            <!--ajouter variable php contributeur connecté-->
-            <!--ajouter variable php contributeur connecté-->
-
             <span><?php echo $_SESSION['pseudo']; ?></span>
             <a class="display_inline_block" href="#"><p>Déconnexion</p></a>
         </div>

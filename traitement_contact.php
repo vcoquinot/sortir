0ba7ee03fac2277bf0b914bc7898f02f4981b2f8
vcoquinot <vcoquinot@gmail.com>
2018-01-25 16:08:57 +0100

    oops ! ai remis ton code !

--- traitement_contact.php
+++ traitement_contact.php
@@ -13,80 +13,27 @@
 
 <body>
 <?php
-$mail = 'vcoquinot@gmail.fr'; // Déclaration de l'adresse de destination.
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
-{
-	$passage_ligne = "\r\n";
-}
-else
-{
-	$passage_ligne = "\n";
-}
-//=====Déclaration des messages au format texte et au format HTML.
-$message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
-$message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-//==========
- 
-//=====Lecture et mise en forme de la pièce jointe.
-//$fichier   = fopen("image.jpg", "r");
-//$attachement = fread($fichier, filesize("image.jpg"));
-//$attachement = chunk_split(base64_encode($attachement));
-//fclose($fichier);
-//==========
- 
-//=====Création de la boundary.
-$boundary = "-----=".md5(rand());
-$boundary_alt = "-----=".md5(rand());
-//==========
- 
-//=====Définition du sujet.
-$sujet = "Hey mon ami !";
-//=========
- 
-//=====Création du header de l'e-mail.
-$header = "From: \"Val\"<vcoquinot@gmail.com>".$passage_ligne;
-$header.= "Reply-to: \"Valb\" <vcoquinot@gmail.com>".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-//==========
- 
-//=====Création du message.
-$message = $passage_ligne."--".$boundary.$passage_ligne;
-$message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
-$message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
-//=====Ajout du message au format texte.
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_txt.$passage_ligne;
-//==========
- 
-$message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
- 
-//=====Ajout du message au format HTML.
-$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_html.$passage_ligne;
-//==========
- 
-//=====On ferme la boundary alternative.
-$message.= $passage_ligne."--".$boundary_alt."--".$passage_ligne;
-//==========
- 
- 
- 
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
- 
-//=====Ajout de la pièce jointe.
-//$message.= "Content-Type: image/jpeg; name=\"image.jpg\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: base64".$passage_ligne;
-//$message.= "Content-Disposition: attachment; filename=\"image.jpg\"".$passage_ligne;
-//$message.= $passage_ligne.$attachement.$passage_ligne.$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne; 
-//========== 
-//=====Envoi de l'e-mail.
-mail($mail,$sujet,$message,$header);
- 
-//==========
+    $destinataire = 'XXXXXX@gmail.com';
+    // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+    $expediteur = $_POST['mail'];
+     
+    $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME Extensions multifonctions du courrier Internet1 c'est un standard internet 
+    $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
+    $headers .= 'To: '.$destinataire."\n"; // Mail de reponse
+    $headers .= 'From: "Nom_de_destinataire"<'.$expediteur.'>'."\n"; // Expediteur
+     
+    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour '.$_POST['nom'].'!<br>
+                    '.$_POST['message'].'</div>';
+     
+    if(mail($destinataire, $message, $headers))
+    {
+        echo '<script languag="javascript" >alert("Votre message a bien été envoyé ");</script>';
+    }
+    else // Non envoyé
+    {
+        echo '<script languag="javascript">alert("Votre message n\'a pas pu être envoyé");</script>';
+    }
+    header('Location: contact.php');
 ?>
 
 
